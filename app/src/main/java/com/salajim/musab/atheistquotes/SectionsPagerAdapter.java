41d3462928c8bf276b2fbf96b003a89ff9de08b9
Musab36 <musabhd@gmail.com>
2017-11-10 17:54:51 +0300

    pagerAdapter setup

--- app/src/main/java/com/salajim/musab/atheistquotes/SectionsPagerAdapter.java
+++ app/src/main/java/com/salajim/musab/atheistquotes/SectionsPagerAdapter.java
@@ -1,8 +1,42 @@
 package com.salajim.musab.atheistquotes;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Musab on 11/10/2017.
  */
 
-public class SectionsPagerAdapter {
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();//Keeps track of the fragments
+    private final List<String> mFragmentTitleList = new ArrayList<>();//Keeps track of the names of the fragments
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //Adds the fragments to the fragment list
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
 }
