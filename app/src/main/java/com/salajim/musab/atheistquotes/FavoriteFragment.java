4d5a5bacbea8466f9b2a15825be09a99465b8d89
Musab36 <musabhd@gmail.com>
2017-11-10 17:18:32 +0300

    setup the fragments

--- app/src/main/java/com/salajim/musab/atheistquotes/FavoriteFragment.java
+++ app/src/main/java/com/salajim/musab/atheistquotes/FavoriteFragment.java
@@ -1,8 +1,25 @@
 package com.salajim.musab.atheistquotes;
 
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Musab on 11/10/2017.
  */
 
-public class FavoriteFragment {
+public class FavoriteFragment extends Fragment {
+    public static final String TAG = "FavoriteFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.favorite_fragment, container, false);
+
+        return view;
+    }
 }
--- app/src/main/java/com/salajim/musab/atheistquotes/TopFragment.java
+++ app/src/main/java/com/salajim/musab/atheistquotes/TopFragment.java
@@ -1,8 +1,24 @@
 package com.salajim.musab.atheistquotes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Musab on 11/10/2017.
  */
 
-public class TopFragment {
+public class TopFragment extends Fragment {
+ public static final String TAG = "TopFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_fragment, container, false);
+
+        return view;
+    }
 }
