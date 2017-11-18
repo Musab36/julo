90ab16835578ba516f237a42a6f11cd7e8299e36
Musab36 <musabhd@gmail.com>
2017-11-18 18:22:12 +0300

    retrieving data from firebase and other small changes

---
+++ app/src/main/java/com/salajim/musab/atheistquotes/ListAdapter.java
@@ -0,0 +1,91 @@
+package com.salajim.musab.atheistquotes;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by Musab on 11/16/2017.
+ */
+
+public class ListAdapter implements android.widget.ListAdapter {
+    private Context context;
+
+    public ListAdapter(Context context) {
+        context = context;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int i) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.reactions, null);
+        }
+
+
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+        TextView likeTextView = (TextView) view.findViewById(R.id.like);
+        TextView commentTextView = (TextView) view.findViewById(R.id.comment);
+
+        return view;
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    }
--- app/src/main/java/com/salajim/musab/atheistquotes/TopFragment.java
+++ app/src/main/java/com/salajim/musab/atheistquotes/TopFragment.java
@@ -6,6 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by Musab on 11/10/2017.
@@ -14,10 +24,41 @@ import android.view.ViewGroup;
 public class TopFragment extends Fragment {
  public static final String TAG = "TopFragment";
 
+    FirebaseDatabase database;
+    DatabaseReference mRef;
+    //List<Quotes> quotes;
+    private ArrayList<String> topQuotes = new ArrayList<>();
+    ListView listView;
+
+    public TopFragment() {}
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.top_fragment, container, false);
+
+        listView = (ListView) view.findViewById(R.id.listView);
+        database = FirebaseDatabase.getInstance();
+        mRef = database.getReference("top");
+
+        mRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
+                    topQuotes.add(dataSnapshot1.getValue().toString());
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, topQuotes);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return view;
     }
