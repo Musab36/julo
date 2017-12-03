503b7fc225b8445159ed8f8d6386584e864f95d1
Musab36 <musabhd@gmail.com>
2017-12-03 20:20:06 +0300

    Floating action button and some few changes

---
+++ app/src/main/java/com/salajim/musab/atheistquotes/AddQuotes.java
@@ -0,0 +1,13 @@
+package com.salajim.musab.atheistquotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddQuotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_quotes);
+    }
+}
---
+++ app/src/main/java/com/salajim/musab/atheistquotes/QuotesAdapter.java
@@ -0,0 +1,8 @@
+package com.salajim.musab.atheistquotes;
+
+/**
+ * Created by Musab on 11/21/2017.
+ */
+
+public class QuotesAdapter {
+}
